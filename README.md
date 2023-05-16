1e5f0bf08b3555547ab319532196de195175c1a5
SebastiaN <odm_sd@hotmail.com>
2023-05-16 16:05:45 -0500

    new: creacion del proyecto

---
+++ README.md
@@ -0,0 +1,12 @@
+# Contenedores Crud
+Tener instalado Docker, para esto debes tener activado la virtualizacion dentro de tu computador
+
+#### Para Correr el contenedor
+````cmd
+docker-compose up --build
+````
+
+#### Para eliminar todos los contenedores, cache, etc.
+````cmd
+docker system prune -a
+````
---
+++ netCore/Clientes/Data/DockerDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace Clientes.Data
+{
+    public class DockerDbContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+        public DockerDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("DockerDb");
+
+        }
+        public IDbConnection CrearConexion() => new SqlConnection(_connectionString);
+    }
+}
